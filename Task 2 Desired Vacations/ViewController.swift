fa5f34ca9fba7cc6625469bfc94dd4a75e535dee
nikolayshiderovGFS <93140449+nikolayshiderovGFS@users.noreply.github.com>
2021-10-27 10:09:33 +0300

    Initial Commit

---
+++ Task 2 Desired Vacations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2 Desired Vacations
+//
+//  Created by Nikolay Shiderov on 27.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
